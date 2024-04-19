6087c9c30d84c969c0395ccb860b21ab4eac23bd
LenDefendel <97990427+LenDefendel@users.noreply.github.com>
2024-04-19 00:03:30 +0000

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,27 +1,43 @@
 import axios from 'axios';
+import type { IMovieShortInfo } from 'src/entities/Movie';
+import type { imdbID } from 'src/types';
 
 const apikey = '771333f5';
 
-export async function getCard(id: `tt${string}`) {
-  return await axios
-    .get('http://www.omdbapi.com/', {
-      params: {
-        apikey,
-        i: id,
-      },
-    })
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .catch((error) => {
-      console.log(error);
-      return Promise.reject();
-    });
+export interface IMovieSearchResponse {
+  Response: 'True';
+  Search: IMovieShortInfo[];
+  totalResults: string;
 }
 
-export async function search(str: string, page: number = 1) {
-  return await axios
+export interface IMovieSearchError {
+  Response: 'False';
+  Error: string;
+}
+
+export async function getCard(id: imdbID) {
+  return (
+    axios
+      .get('http://www.omdbapi.com/', {
+        params: {
+          apikey,
+          i: id,
+        },
+      })
+      .then((response) => {
+        // console.log(response);
+        return response;
+      })
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .catch((error) => {
+        // console.log(error);
+        return Promise.reject();
+      })
+  );
+}
+
+export async function searchMovieByString(str: string, page: number = 1) {
+  return axios
     .get('http://www.omdbapi.com/', {
       params: {
         apikey,
@@ -29,12 +45,15 @@ export async function search(str: string, page: number = 1) {
         page,
       },
     })
-    .then((response) => {
-      console.log(response);
-      return response;
+    .then((response) => response.data)
+    .then((data: IMovieSearchResponse | IMovieSearchError) => {
+      if (data.Response === 'False') {
+        throw new Error(data.Error);
+      }
+
+      return data;
     })
     .catch((error) => {
-      console.log(error);
-      return Promise.reject();
+      return Promise.reject(error);
     });
 }
