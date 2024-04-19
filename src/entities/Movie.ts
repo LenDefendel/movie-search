acdcf6cc4e16eaf2ae2cbbb532be063daac85fd3
LenDefendel <97990427+LenDefendel@users.noreply.github.com>
2024-04-19 00:02:38 +0000

    Rename entity Movie

---
+++ src/entities/Movie.ts
@@ -0,0 +1,42 @@
+import type { httpLink, imdbID } from 'src/types';
+
+export interface IMovieLongInfo {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: httpLink;
+  Ratings: IRating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: imdbID;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+}
+
+export interface IMovieShortInfo {
+  Title: string;
+  Year: string;
+  imdbID: imdbID;
+  Type: string;
+  Poster: httpLink;
+}
+
+export interface IRating {
+  Source: string;
+  Value: string;
+}
