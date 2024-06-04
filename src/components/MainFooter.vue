7df4f38a8414045c3a7d72ab090d8ddac1b9ce12
LenDefendel <97990427+LenDefendel@users.noreply.github.com>
2024-06-04 01:24:44 +0000

    Update MainFooter btn github

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,13 +1,15 @@
 <template>
   <q-footer class="footer">
-    <q-tabs no-caps indicator-color="transparent">
-      <a
-        class="github"
-        href="https://github.com/LenDefendel?tab=overview&from=2025-08-01&to=2025-08-01"
-      >
-        <q-tab name="your github" label="your github" />
-      </a>
-    </q-tabs>
+    <q-btn
+      class="github"
+      label="your github"
+      href="https://github.com/LenDefendel?tab=overview&from=2025-08-01&to=2025-08-01"
+      flat
+      unelevated
+      square
+      :ripple="false"
+      no-caps
+    />
   </q-footer>
 </template>
 
@@ -15,11 +17,12 @@
 //
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer {
   display: flex;
   justify-content: center;
   background-color: var(--panel-background);
+  height: 50px;
 }
 
 .github {
